116bffd22ed618ad1699e287fee1cbc080dd377c
Steve Butler <butlersteve.c@gmail.com>
2023-11-11 12:58:15 -0600

    add basic CRUD operation endpoints for Person, add checkUser for front-end auth

---
+++ Migrations/20231111173347_updateTeamICollection.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace people_dot_org.Migrations
+{
+    public partial class updateTeamICollection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PersonTeam_People_PersonsId",
+                table: "PersonTeam");
+
+            migrationBuilder.RenameColumn(
+                name: "PersonsId",
+                table: "PersonTeam",
+                newName: "PeopleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PersonTeam_People_PeopleId",
+                table: "PersonTeam",
+                column: "PeopleId",
+                principalTable: "People",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PersonTeam_People_PeopleId",
+                table: "PersonTeam");
+
+            migrationBuilder.RenameColumn(
+                name: "PeopleId",
+                table: "PersonTeam",
+                newName: "PersonsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PersonTeam_People_PersonsId",
+                table: "PersonTeam",
+                column: "PersonsId",
+                principalTable: "People",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Http.Json;
 using people_dot_org;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
@@ -50,6 +51,68 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//  ### USER Endpoints ###
+
+//CHECK USER EXISTS
+app.MapGet("/api/checkuser/{uid}", (PeopleDotOrgDbContext db, string uid) =>
+{
+    var userExists = db.People.Where(x => x.Uid == uid).FirstOrDefault();
+    if (userExists == null)
+    {
+        return Results.StatusCode(204);
+    }
+    return Results.Ok(userExists);
+});
+
+//Get All People
+app.MapGet("/api/people", (PeopleDotOrgDbContext db) =>
+{
+    return db.People.ToList();
+});
+
+//Create Person
+app.MapPost("/api/person", (PeopleDotOrgDbContext db, Person person) =>
+{
+    db.People.Add(person);
+    db.SaveChanges();
+    return Results.Created($"/api/people/{person.Id}", person);
+});
+
+//Edit Person
+app.MapPut("/api/person/{personId}", (PeopleDotOrgDbContext db, int id, Person person) =>
+{
+    var personToUpdate = db.People.FirstOrDefault(x => x.Id == id);
+    if (personToUpdate == null)
+    {
+        return Results.NotFound();
+    }
+
+    personToUpdate.Id = person.Id;
+    personToUpdate.FirstName = person.FirstName;
+    personToUpdate.LastName = person.LastName;
+    personToUpdate.Email = person.Email;
+    personToUpdate.Phone = person.Phone;
+    personToUpdate.IsAdmin = person.IsAdmin;
+    personToUpdate.IsTeamLead = person.IsTeamLead;
+
+    db.SaveChanges();
+
+    return Results.NoContent();
+});
+
+
+// Delete an user
+app.MapDelete("/api/person/{personId}", (PeopleDotOrgDbContext db, int id) =>
+{
+    Person person = db.People.SingleOrDefault(u => u.Id == id);
+    if (person == null)
+    {
+        return Results.NotFound();
+    }
+    db.People.Remove(person);
+    db.SaveChanges();
+    return Results.NoContent();
+});
 
 
 app.Run();
